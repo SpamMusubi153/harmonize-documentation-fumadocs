a9f398662450faa064960ce78932b9539b150a40
Max Tran <76268730+SpamMusubi153@users.noreply.github.com>
2025-08-26 11:28:53 -0700

    Enable Static Export Option for Next.js

--- harmonize/next.config.mjs
+++ harmonize/next.config.mjs
@@ -3,6 +3,7 @@ import { withContentCollections } from '@content-collections/next';
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+  output: 'export',
 };
 
 export default withContentCollections(config);
