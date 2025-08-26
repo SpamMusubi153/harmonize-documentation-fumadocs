22daecc70919174ee8c2e73b8995b29f3332ef3d
Max Tran <76268730+SpamMusubi153@users.noreply.github.com>
2025-08-26 16:56:24 +0000

    Update Site With Support for Python Documentation

--- harmonize/src/lib/source.ts
+++ harmonize/src/lib/source.ts
@@ -2,7 +2,33 @@ import { allDocs, allMetas } from 'content-collections';
 import { loader } from 'fumadocs-core/source';
 import { createMDXSource } from '@fumadocs/content-collections';
 
+import { allApiDocs } from 'content-collections';
+import { createJSONSource } from '../../sphinx/source';
+
+// console.log(allMetas)
+
+let mdxSource = createMDXSource(allDocs, allMetas);
+let jsonSource = createJSONSource(allApiDocs, allMetas);
+
+let unifiedSource = {
+  files: [
+    ...mdxSource.files,
+    ...jsonSource,
+  ]
+}
+
+// console.log(mdxSource);
+// console.log(jsonSource);
+// console.log(unifiedSource);
+
 export const source = loader({
   baseUrl: '/docs',
-  source: createMDXSource(allDocs, allMetas),
+  source: unifiedSource,
 });
+
+// import { createJSONSource } from '../../sphinx/helper';
+// import { allApiDocs } from 'content-collections';
+// export const source2 = loader({
+//   baseUrl: '/apiDocs',
+//   source: createMDXSource(allApiDocs, allMetas),
+// });
