a02ce0a6264056995469f2a71013d1b5de08c167
Max Tran <76268730+SpamMusubi153@users.noreply.github.com>
2025-08-27 16:25:28 +0000

    Unify Routing under /docs under a single Dynamic Route Segment

--- harmonize/src/app/docs/[[...slug]]/page.tsx
+++ harmonize/src/app/docs/[[...slug]]/page.tsx
@@ -6,33 +6,74 @@ import {
   DocsDescription,
 } from 'fumadocs-ui/page';
 import { notFound } from 'next/navigation';
+
 import { MDXContent } from '@content-collections/mdx/react';
 import { createRelativeLink } from 'fumadocs-ui/mdx';
 import { getMDXComponents } from '@/mdx-components';
 
-export default async function Page(props: {
-  params: Promise<{ slug?: string[] }>;
-}) {
+export default async function Page( props: { params: Promise<{ slug?: string[] }>;} ) {
+  
   const params = await props.params;
+
   const page = source.getPage(params.slug);
-  // console.log(`In /docs; slug: ${params.slug} --- ${params.slug?.join("/")}`);
   if (!page) notFound();
 
-  return (
-    <DocsPage toc={page.data.toc} full={page.data.full}>
-      <DocsTitle>{page.data.title}</DocsTitle>
-      <DocsDescription>{page.data.description}</DocsDescription>
-      <DocsBody>
-        <MDXContent
-          code={page.data.body}
-          components={getMDXComponents({
-            // this allows you to link to other pages with relative file paths
-            a: createRelativeLink(source, page),
-          })}
-        />
-      </DocsBody>
-    </DocsPage>
-  );
+  const pageExtension = page?.data._meta.extension;
+  
+  console.log(`Under /docs. The current slug is: [${params.slug}]`);
+
+  if (pageExtension == "mdx"){
+    return (
+      <DocsPage toc={page.data.toc} full={page.data.full}>
+        <DocsTitle>{page.data.title}</DocsTitle>
+        <DocsDescription>{page.data.description}</DocsDescription>
+        <DocsBody>
+          <MDXContent
+            code={page.data.body}
+            components={getMDXComponents({
+              // this allows you to link to other pages with relative file paths
+              a: createRelativeLink(source, page),
+            })}
+          />
+        </DocsBody>
+      </DocsPage>
+    );
+  }
+
+  else if (pageExtension == "fjson"){
+    return (
+      // <DocsPage toc={page.data.toc} full={page.data.full}>
+      <DocsPage full={page.data.full}>
+        <DocsTitle>{page.data.title}</DocsTitle>
+        <DocsDescription>{page.data.description}</DocsDescription>
+        <DocsBody>
+
+          {/* <br></br> */}
+          {/* <hr></hr> */}
+
+          {/* <h3>At a Glance</h3> */}
+
+          {/* Table of Contents */}
+          <div dangerouslySetInnerHTML={{__html: page.data.htmltoc}}></div>
+
+          <hr></hr>
+
+          <h2>The Details</h2>
+
+          {/* Sphinx-Generated HTML */}
+          <div dangerouslySetInnerHTML={{__html: page.data.body}}></div>
+
+          {/* <MDXContent
+            code={page.data.body}
+            components={getMDXComponents({
+              // this allows you to link to other pages with relative file paths
+              a: createRelativeLink(source, page),
+            })}
+          /> */}
+        </DocsBody>
+      </DocsPage>
+    );
+  }  
 }
 
 export function generateStaticParams() {
