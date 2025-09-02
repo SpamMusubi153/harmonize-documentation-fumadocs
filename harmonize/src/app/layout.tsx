939fcb311185347eeaedd848a9261d576198ac00
Max Tran <76268730+SpamMusubi153@users.noreply.github.com>
2025-09-02 22:26:43 +0000

    Refactor Search Organization

--- harmonize/src/app/layout.tsx
+++ harmonize/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import '@/app/global.css';
-import { RootProvider } from 'fumadocs-ui/provider';
+// import { RootProvider } from 'fumadocs-ui/provider';
+import { Provider } from '@/components/provider';
 import { Inter } from 'next/font/google';
 import type { ReactNode } from 'react';
 
@@ -11,7 +12,11 @@ export default function Layout({ children }: { children: ReactNode }) {
   return (
     <html lang="en" className={inter.className} suppressHydrationWarning>
       <body className="flex flex-col min-h-screen">
-        <RootProvider>{children}</RootProvider>
+        <Provider>
+        {/* <RootProvider> */}
+          {children}
+        {/* </RootProvider> */}
+        </Provider>
       </body>
     </html>
   );
