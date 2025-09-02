ef3cebe73411bf6998f93f958db5dd3a0d8ccbf7
Max Tran <76268730+SpamMusubi153@users.noreply.github.com>
2025-09-02 18:56:35 +0000

    Update Base Export Path

--- harmonize/next.config.mjs
+++ harmonize/next.config.mjs
@@ -2,6 +2,7 @@ import { withContentCollections } from '@content-collections/next';
 
 /** @type {import('next').NextConfig} */
 const config = {
+  basePath: "/harmonize-documentation-fumadocs",
   reactStrictMode: true,
   output: 'export',
   // eslint: {
