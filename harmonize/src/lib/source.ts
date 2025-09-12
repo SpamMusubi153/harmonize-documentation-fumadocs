05cf1e9c3803d76cfffdd0ef2031b22459c457da
Max Tran <76268730+SpamMusubi153@users.noreply.github.com>
2025-09-12 16:27:41 +0000

    Support Loading Icons by the Loader

--- harmonize/src/lib/source.ts
+++ harmonize/src/lib/source.ts
@@ -5,6 +5,9 @@ import { createMDXSource } from '@fumadocs/content-collections';
 import { allApiDocs } from 'content-collections';
 import { createJSONSource } from '../../sphinx/source';
 
+import { icons } from 'lucide-react'
+import { createElement } from 'react';
+
 const mdxSource = createMDXSource(allDocs, allMetas);
 const jsonSource = createJSONSource(allApiDocs, allMetas);
 
@@ -25,4 +28,15 @@ const unifiedSource = {
 export const source = loader({
   baseUrl: '/docs',
   source: unifiedSource,
+
+  // Support Loading Lucide Icons
+  icon: (icon) => {
+    if (!icon) {
+      return;
+    }
+
+    if (icon in icons) {
+      return createElement(icons[icon as keyof typeof icons]);
+    }
+  }
 });
