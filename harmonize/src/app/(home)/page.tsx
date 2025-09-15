8076ff924ebcbe46cb721edf8464ba02990a0202
Max Tran <76268730+SpamMusubi153@users.noreply.github.com>
2025-09-15 11:11:40 -0700

    Update Landing Page

--- harmonize/src/app/(home)/page.tsx
+++ harmonize/src/app/(home)/page.tsx
@@ -3,9 +3,11 @@ import Link from 'next/link';
 export default function HomePage() {
   return (
     <main className="flex flex-1 flex-col justify-center text-center">
-      <h1 className="mb-4 text-2xl font-bold">Hello World</h1>
+      <h1 className="mb-4 text-2xl font-bold">Welcome!</h1>
+      <p>A new landing page is on the way.</p>
+      <br>
       <p className="text-fd-muted-foreground">
-        Visit our documentation at {' '}
+        In the meantime, visit our documentation at {' '}
         <Link
           href="/docs"
           className="text-fd-foreground font-semibold underline"
