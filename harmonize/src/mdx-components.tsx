746302108b505347446d8e339e41718726002a0a
Max Tran <76268730+SpamMusubi153@users.noreply.github.com>
2025-09-17 12:08:37 -0700

    Update List of Included Components

--- harmonize/src/mdx-components.tsx
+++ harmonize/src/mdx-components.tsx
@@ -1,13 +1,19 @@
 import defaultMdxComponents from 'fumadocs-ui/mdx';
-import * as TabsComponents from 'fumadocs-ui/components/tabs'
-import { Mermaid } from '@/components/mdx/mermaid'
+import * as TabsComponents from 'fumadocs-ui/components/tabs';
+import { Accordion, Accordions } from "./components/accordion";
+import { Step, Steps } from "fumadocs-ui/components/steps";
+import { Mermaid } from '@/components/mdx/mermaid';
 import type { MDXComponents } from 'mdx/types';
 
 // use this function to get MDX components, you will need it for rendering MDX
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
     ...defaultMdxComponents,
-    ... TabsComponents,
+    ...TabsComponents,
+    Accordion,
+    Accordions,
+    Step,
+    Steps,
     Mermaid,
     ...components,
   };
